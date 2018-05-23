24a69e75425b22e6545d44f499ae342e922e4d33
adi elbaz <adielbaz25@gmail.com>
2018-05-23 15:52:16 +0300

    add delete button for boards

---
+++ src/app/Dialogs/edit-board-dialog/edit-board-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-board-dialog',
+  templateUrl: './edit-board-dialog.component.html',
+  styleUrls: ['./edit-board-dialog.component.css']
+})
+export class EditBoardDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
